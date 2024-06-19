981a676340ed83435d39eedd2dc7cc0e73220063
her97 <simkoon0120@gmail.com>
2024-06-19 22:38:37 +0900

    second-commit

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 // import { store } from './app/store';
 import App from './App';
 
-import './basic-layout.css';
+import './layout.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
